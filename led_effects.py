e4384a808cbc91f9b0e7c3f269068e65db1d0d9b
Artem <a4b11s@gmail.com>
2025-02-07 00:23:58 +0200

    Refactor LED controller to implement an interface and enhance effect handling

--- led_effects.py
+++ led_effects.py
@@ -2,8 +2,26 @@ import neopixel
 from config import LED_PIN, NUM_LEDS  # Import configuration
 
 
-class LedController:
-    def __init__(self, pin=LED_PIN, num_leds=NUM_LEDS):  # Use default config values
+class LedControllerInterface:
+    num_leds: int
+    pin: int
+    pixels: neopixel.NeoPixel
+
+    def fill(self, color):
+        raise NotImplementedError
+
+    def clear(self):
+        raise NotImplementedError
+
+    def write(self):
+        raise NotImplementedError
+
+    def __setitem__(self, key, value):
+        raise NotImplementedError
+
+
+class LedController(LedControllerInterface):
+    def __init__(self, pin, num_leds):
         self.pixels = neopixel.NeoPixel(pin, num_leds)
         self.num_leds = num_leds
         self.clear()
@@ -15,9 +33,15 @@ class LedController:
     def clear(self):
         self.fill((0, 0, 0))
 
+    def write(self):
+        self.pixels.write()
+
+    def __setitem__(self, key, value):
+        self.pixels[key] = value  # type: ignore
+
 
 class BaseEffect:
-    def __init__(self, controller: LedController):
+    def __init__(self, controller: LedControllerInterface):
         self.controller = controller
 
     def apply(self, *args, **kwargs):
@@ -26,26 +50,37 @@ class BaseEffect:
 
 class PulseEffect(BaseEffect):
     def apply(self, color, step=0):
-        # Normalize step to be within 0-511
-        step = step % 512
-        # Reflect step to create a pulsing effect
-        step = step if step < 256 else 511 - step
-        # Scale color by the step value
-        color = tuple(int(c * step / 255) for c in color)
+        step = self._normalize_step(step)
+        color = self._scale_color(color, step)
         self.controller.fill(color)
+
+    def _normalize_step(self, step):
+        return step % 512
+
+    def _scale_color(self, color, step):
+        step = step if step < 256 else 511 - step
+        return tuple(int(c * step / 255) for c in color)
 
 
 class SnakeEffect(BaseEffect):
     def apply(self, color, length=5, step=0, *args, **kwargs):
+        step, backward = self._calculate_step(step, length)
+        self.controller.clear()
+        self._draw_snake(color, length, step, backward)
+        self.controller.write()
+
+    def _calculate_step(self, step, length):
         step = step % (self.controller.num_leds + length)
         backward = step >= self.controller.num_leds
         step = step % self.controller.num_leds
-        self.controller.clear()
+        return step, backward
+
+    def _draw_snake(self, color, length, step, backward):
         for j in range(length):
             index = step - j if not backward else step + j
             if 0 <= index < self.controller.num_leds:
-                self.controller.pixels[index] = color # type: ignore
-        self.controller.pixels.write()
+                self.controller[index] = color  # type: ignore
+
 
 class FillEffect(BaseEffect):
     def apply(self, color, *args, **kwargs):
