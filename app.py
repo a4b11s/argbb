cf967d684d207d19b8455307747a810e9b3c883c
Artem <a4b11s@gmail.com>
2025-02-07 00:45:53 +0200

    Refactor button handling; introduce Button class for improved encapsulation and functionality

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from led_effects.led_controller import LedController
 from modes.mode_controller import ModeController
 import utime
 from config import DEBUG_MODE
-
+from button import Button
 
 class App:
     button_config = {
@@ -24,46 +24,15 @@ class App:
 
     colors_list = list(colors.values())
 
-    def __init__(self, controller: LedController, button: Pin):
+    def __init__(self, controller: LedController, button_pin: Pin):
         self.controller = controller
-        self.button = button
+        self.button = Button(button_pin, self.button_config)
         self.is_sleeping = False
-
-        self.has_been_pressed = False
-        self.is_pressed = False
 
         self.colors_pointer = 0
 
         self.modes = ModeController(controller)
-        self.pressed_at = 0
-        self.pressed_time = 0
-
         self.start_time = utime.ticks_ms()
-
-    def on_pressed(self):
-        self.pressed_time = 0
-        self.has_been_pressed = True
-        self.is_pressed = True
-        self.pressed_at = utime.ticks_ms()
-
-    def on_released(self):
-        self.is_pressed = False
-        self.pressed_time = utime.ticks_diff(utime.ticks_ms(), self.pressed_at)
-        self.pressed_at = 0
-
-    def check_button(self):
-        if self.button.value() == 0:
-            if not self.is_pressed:
-                self.on_pressed()
-            elif (
-                utime.ticks_diff(utime.ticks_ms(), self.pressed_at)
-                > self.button_config["off"]
-            ):
-                self._off()
-
-        else:
-            if self.is_pressed:
-                self.on_released()
 
     def change_speed(self):
         self.modes.change_speed()
@@ -83,21 +52,6 @@ class App:
     def _on_very_long_press(self):
         self.change_mode()
 
-    def _off(self):
-        raise Exception("OFF REQUESTED")
-
-    def button_process(self, threshold=10):
-        self.check_button()
-        if self.pressed_time > threshold and self.has_been_pressed:
-            self.has_been_pressed = False
-
-            if self.pressed_time < self.button_config["long_press"]:
-                self._on_short_press()
-            elif self.pressed_time < self.button_config["very_long_press"]:
-                self._on_long_press()
-            else:
-                self._on_very_long_press()
-
     def run(self):
         self.start_time = utime.ticks_ms()
 
@@ -109,7 +63,11 @@ class App:
             current_mode = self.modes.get_current_mode()
             current_mode.apply(color, time_pointer)
 
-            self.button_process()
+            self.button.button_process(
+                on_short_press=self._on_short_press,
+                on_long_press=self._on_long_press,
+                on_very_long_press=self._on_very_long_press
+            )
 
             if DEBUG_MODE:
                 self._print_debug()
@@ -119,8 +77,8 @@ class App:
         string = f"Speed: {self.modes.get_speed()},"
         string += f" Mode: {self.modes.get_current_mode().name},"
         string += f" Color: {self.colors_list[self.colors_pointer]},"
-        string += f" Pressed: {self.is_pressed},"
-        string += f" Pressed Time: {self.pressed_time}"
+        string += f" Pressed: {self.button.is_pressed},"
+        string += f" Pressed Time: {self.button.pressed_time}"
         string += "\r"
 
         print(
