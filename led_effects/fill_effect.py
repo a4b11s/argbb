edd3993a435b537d9d096f00acd32ac78c61846d
Artem <a4b11s@gmail.com>
2025-02-08 21:30:06 +0200

    Add FillEffect class with asynchronous execution for color filling

--- led_effects/fill_effect.py
+++ led_effects/fill_effect.py
@@ -1,1 +1,12 @@
 from led_effects.effect import Effect
+import asyncio
+
+
+class FillEffect(Effect):
+    async def run(self, color, sleep_ms):
+        self.strip.fill(color)
+        self.strip.write()
+        await self._sleep(sleep_ms)
+
+    async def _sleep(self, sleep_ms):
+        await asyncio.sleep(sleep_ms / 1000)
