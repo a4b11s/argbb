e663651162add613c71f5ac57b59e6e5cfe97604
Artem <a4b11s@gmail.com>
2025-02-08 21:32:43 +0200

    Remove unused asyncio import and redundant sleep call in FillEffect class

--- led_effects/fill_effect.py
+++ led_effects/fill_effect.py
@@ -1,9 +1,7 @@
 from led_effects.effect import Effect
-import asyncio
 
 
 class FillEffect(Effect):
     async def run(self, color, sleep_ms):
         self.strip.fill(color)
         self.strip.write()
-        await self._sleep(sleep_ms)
