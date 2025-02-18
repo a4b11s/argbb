9cd0a09b695d0704a1f21b163651cbf234ec0a4d
Artem <a4b11s@gmail.com>
2025-02-18 23:12:48 +0200

    Refactor Button class to improve press detection and state management

--- button.py
+++ button.py
@@ -7,28 +7,45 @@ import asyncio
 class Button:
     def __init__(self, button: Pin):
         self.button = button
-        self.pressed_times = 0
-        self.last_pressed_state = False
-        self.pressed_times = 0
+        self._pressed_times = 0
+        self._is_pressed = False
+        self._has_been_pressed = False
+        self._pressed_at = 0
+        self._pressed_time = 0
+        self._last_pressed_time = 0
+        self._pressed_times_threshold = 300
 
-    def _check(self):
-        return self.button.value()
+    def _count_presses(self):
+        if self._last_pressed_time > self._pressed_times_threshold:
+            self._pressed_times = 0
+        else:
+            self._pressed_times += 1
 
     @property
     def is_pressed_now(self):
-        return self._check() == 0
+        return self.button.value() == 0
 
     async def _loop(self):
         while True:
-            if self.is_pressed_now and not self.last_pressed_state:
-                self._on_press()
-            elif not self.is_pressed_now and self.last_pressed_state:
-                self._on_release()
+            self._check()
             await asyncio.sleep(0)
 
+    def _check(self):
+        if self.is_pressed_now and not self._has_been_pressed:
+            self._on_press()
+        elif not self.is_pressed_now and self._has_been_pressed:
+            self._on_release()
+
     def _on_press(self):
-        self.pressed_times += 1
-        self.last_pressed_state = True
+        self._last_pressed_time = self._pressed_time
+        self._count_presses()
+
+        self._pressed_time = 0
+        self._has_been_pressed = True
+        self._is_pressed = True
+        self._pressed_at = utime.ticks_ms()
 
     def _on_release(self):
-        self.last_pressed_state = False
+        self._is_pressed = False
+        self._pressed_time = utime.ticks_diff(utime.ticks_ms(), self._pressed_at)
+        self._pressed_at = 0
