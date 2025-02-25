6d4095927d65259427de579c40243d5a6f5885ab
Artem <a4b11s@gmail.com>
2025-02-25 15:59:24 +0200

    Refactor MeteorEffect class to allow customizable trail length, fade factor, and spark probability

--- led_effects/meteor_effect.py
+++ led_effects/meteor_effect.py
@@ -3,18 +3,17 @@ import random
 
 
 class MeteorEffect(Effect):
-    def __init__(self, strip):
+    def __init__(self, strip, trail_length=50, fade_factor=(0.7, 0.9), spark_probability=0.1):
         super().__init__(strip)
         self.trail_length = 50
-        self.fade_factor = (0.8, 0.9)
-        self.spark_probability = 0.1
+        self.fade_factor = fade_factor
+        self.spark_probability = spark_probability
 
     async def run(self, color, sleep_ms):
-        NUM_PIXELS = len(self.strip)
-        pixels = [(0, 0, 0)] * NUM_PIXELS
+        pixels = [(0, 0, 0)] * len(self.strip)
 
-        for i in range(NUM_PIXELS + self.trail_length):
-            if i < NUM_PIXELS:
+        for i in range(len(self.strip) + self.trail_length):
+            if i < len(self.strip):
                 pixels[i] = color
 
             self._apply_fade_effect(pixels)
@@ -24,11 +23,8 @@ class MeteorEffect(Effect):
 
     def _apply_fade_effect(self, pixels):
         for j in range(len(pixels)):
-            is_spark = random.random() < self.spark_probability
-            fade_multiplayer = random.uniform(*self.fade_factor)
 
-            if is_spark:
-                fade_multiplayer = 1 + 1 - fade_multiplayer
+            fade_multiplayer = self._calc_fade_multiplayer()
 
             r, g, b = pixels[j]
             pixels[j] = (
@@ -37,9 +33,16 @@ class MeteorEffect(Effect):
                 int(b * fade_multiplayer),
             )
 
-    def _update_strip(self, pixels):
-        # pixels = pixels.reverse()
+    def _calc_fade_multiplayer(self):
+        fade_multiplayer = random.uniform(*self.fade_factor)
 
+        if random.random() < self.spark_probability:
+            fade_multiplayer = 2 - fade_multiplayer
+
+        return fade_multiplayer
+
+    def _update_strip(self, pixels):
         for k in range(len(self.strip)):
             self.strip[k] = pixels[k]
+
         self.strip.write()
