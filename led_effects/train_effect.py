2bf12ce4d4db46991ed75e00a1dd2d28d1a25c74
Artem <a4b11s@gmail.com>
2025-04-15 16:23:26 +0300

    Refactor TrainEffect to correctly calculate shifts using gap

--- led_effects/train_effect.py
+++ led_effects/train_effect.py
@@ -8,12 +8,12 @@ class TrainEffect(Effect):
         color = self.config.get("primary_color")
         bg_color = self.config.get("bg_color")
         sleep_ms = int(self.config.get("sleep_ms"))
-        for shift in range(car_size + 1):
+        for shift in range(car_size + gap):
             for i in range(len(self.strip)):
-                if (i - shift) % car_size == 0:
-                    self.strip[i] = bg_color
-                else:
+                if (i - shift) % (car_size + gap) < car_size:
                     self.strip[i] = color
+                else:
+                    self.strip[i] = bg_color
             self.strip.write()
 
             await self._sleep(sleep_ms)
